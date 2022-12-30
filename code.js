81c0eb941ff8a375de81d1656f0c11f8b1fedb8c
Stephen Rice <goatwipe@gmail.com>
2022-12-30 10:26:55 +0000

    initial files give a drawing canvas

---
+++ code.js
@@ -0,0 +1,6 @@
+// a handle for the drawing canvas object
+const feigenCanvas = document.getElementById("feigenbaum-diagram");
+// a context for the drawing canvas object with properties and methods for drawing
+const feigenContext = feigenCanvas.getContext("2d");
+// test drawing
+feigenContext.fillStyle = "#FF0000";
