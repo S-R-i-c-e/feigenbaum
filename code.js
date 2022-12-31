014761de56c429fe07e17fada79da05bcf32463f
Stephen Rice <goatwipe@gmail.com>
2022-12-31 12:00:01 +0000

    working code - conversion from function ranges to canvas pixel

--- code.js
+++ code.js
@@ -3,71 +3,45 @@ const feigenCanvas = document.getElementById("feigenbaum-diagram");
 // a context for the drawing canvas object with properties and methods for drawing
 const feigenContext = feigenCanvas.getContext("2d");
 
-const toOneDP = function (number) {
-    return Math.round(number*10)/10;
+feigenContext.font = "9px Arial";
+
+function roundDP(number, accuracy) {
+    const mult = 10 ** accuracy;
+    return Math.round(number * mult) / mult;
+  }
+
+// system parameters
+const canvasWidth = feigenCanvas.width;
+const canvasHeight = feigenCanvas.height;
+
+//this app parameters
+const funcRangeX = 3;
+const funcRangeY = 1.5;
+
+const deltaX = funcRangeX/canvasWidth;
+const deltaY = funcRangeY/canvasHeight;
+
+function canvasX(x) {
+    return Math.round(x/deltaX);
+}
+function canvasY(y) {
+    return canvasHeight - Math.round((y/deltaY));
 }
 
-// notional coordinate space
-const xPixels = 500;
-const yPixels = 500;
-
-// canvas space
-const systemWidth = feigenCanvas.width;
-const systemHeight = feigenCanvas.height;
-
-// canvas pixels
-const deltaX = toOneDP(systemWidth/xPixels);
-const deltaY = toOneDP(systemHeight/yPixels);
-
-// convert notional space to system space
-function systemY(y) {
-    return systemHeight-(y*deltaY);
-}
-
-function systemX(x) {
-    return x*deltaX;
-}
-
-// set notional pixel -> canvas coordinates and set
 function setPixel(x,y) {
-    let canvasY = systemY(y);
-    let canvasX = systemX(x);
-    feigenContext.fillRect(canvasX,canvasY,deltaX,deltaY);
-    return true;
+    feigenContext.fillRect(canvasX(x),canvasY(y),1,1);
 }
 
-y=100;
-for (let x=100;x<=200;x++) {
-    setPixel(x,y);
+function feigen(population, feedback) {
+    return population + population * feedback * (1-population);
 }
-x=100;
-for (let y=100;y<=200;y++) {
-    setPixel(x,y);
+
+for (range=deltaX;range<funcRangeX;range+=deltaX) {
+    feed = range;
+    numIterations = 50;
+    pop = 0.3;
+    for (index=1;index<numIterations;index++) {
+        setPixel(feed,pop);
+        pop = feigen(pop, feed);
+    }
 }
-x=200;
-for (let y=100;y<=300;y++) {
-    setPixel(x,y);
-}
-feigenContext.font = "16px Arial";
-feigenContext.fillText(deltaX, 300, 100);
-// screen constants
-// const xPixels = 500;
-// const yPixels = 500;
-
-// // drawing constants
-// const deltaX = toOneDP(feigenCanvas.width/xPixels);
-// const deltaY = toOneDP(feigenCanvas.height/yPixels);
-
-// // test drawing
-// feigenContext.fillStyle = "#FF0000";
-// // test drawing
-
-// let y=200;
-// for (let x=200; x<300; x++) {
-//     drawPixel(x,y);
-// }
-
-// function drawPixel(x, y) {
-//     feigenContext.fillRect(x,y,1,1);
-//     return true
-// }
